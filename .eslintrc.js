99b6dc7ec3f78abae5c7d42583177fff87317646
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-06-07 13:06:46 +0200

    Add eslint stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,26 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
-    },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
   ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'eslint-plugin-react-hooks',
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
 };
