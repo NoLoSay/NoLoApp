59b5898c66114dd531b26d3f796e8514fd6f251c
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-07-13 17:56:00 +0200

    Add global files

---
+++ src/global/colors.ts
@@ -0,0 +1,23 @@
+const mainColors = {
+  accent: '#FDC810',
+  black: '#000000',
+  white: '#FFFFFF',
+  dark: '#343434',
+  lightGrey: '#CFCFCF',
+  darkGrey: '646464',
+}
+
+const textColors = {
+  main: mainColors.dark,
+  light: mainColors.lightGrey,
+}
+
+export const colors = {
+  accent: mainColors.accent,
+  black: mainColors.black,
+  white: mainColors.white,
+  dark: mainColors.dark,
+  lightGrey: mainColors.lightGrey,
+  darkGrey: mainColors.darkGrey,
+  text: textColors,
+}
---
+++ src/global/images.ts
@@ -0,0 +1,14 @@
+export const images = {
+  icons: {
+    full: {
+      add: require('@assets/icons/full/add.png'),
+      home: require('@assets/icons/full/home.png'),
+      qr: require('@assets/icons/full/qr.png'),
+    },
+    outline: {
+      add: require('@assets/icons/outline/add.png'),
+      home: require('@assets/icons/outline/home.png'),
+      qr: require('@assets/icons/outline/qr.png'),
+    },
+  },
+}
