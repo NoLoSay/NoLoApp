da35a4a47efc899bde410c194ffbc3b4262e11a3
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-07-13 17:53:11 +0200

    Refactor tests architecture

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import App from '../App'
+import App from '@source/AuthRouter'
 import { render, screen } from '@testing-library/react-native'
 
 test('renders correctly', () => {
   render(<App />)
 
-  expect(screen.getByText('App.tsx'))
+  expect(screen.getByText('Open up App.tsx to start working on your app!'))
 })
