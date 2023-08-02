e82dce7d60958c4cc8a31b4c9b4665bdb17566d3
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-08-02 13:22:50 +0200

    Add images loading

--- src/global/images.ts
+++ src/global/images.ts
@@ -11,12 +11,25 @@ export const images = {
       add: require('@assets/icons/full/add.png'),
       home: require('@assets/icons/full/home.png'),
       qr: require('@assets/icons/full/qr.png'),
+      shield: require('@assets/icons/full/shield.png'),
+      user: require('@assets/icons/full/user.png'),
+      eye: require('@assets/icons/full/eye.png'),
     },
     outline: {
       add: require('@assets/icons/outline/add.png'),
       home: require('@assets/icons/outline/home.png'),
       qr: require('@assets/icons/outline/qr.png'),
     },
+    social: {
+      facebook: require('@assets/icons/social/facebook.png'),
+      google: require('@assets/icons/social/google.png'),
+      apple: require('@assets/icons/social/apple.png'),
+    },
+  },
+  logos: {
+    nolosay: require('@assets/images/logos/nolosay.png'),
+    heart: require('@assets/images/logos/heart.png'),
+    logo: require('@assets/images/logos/logo.png'),
   },
 }
 
