c4919c8ccaa1b5aeeb994391b32436a40de5d34f
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-09-08 19:01:06 -0400

    Fix connection screen test

--- __tests__/screens/authentificationSection/connection/ConnectionScreen.test.tsx
+++ __tests__/screens/authentificationSection/connection/ConnectionScreen.test.tsx
@@ -4,6 +4,18 @@ import ConnectionScreen from '@source/screens/authentificationSection/connection
 import { AuthStackParamList } from '@source/global/types/screensProps/AuthStackParams'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
+jest.mock('react-native', () => {
+  const RN = jest.requireActual('react-native')
+
+  RN.NativeModules.SettingsManager = {
+    settings: {
+      AppleLocale: 'en-US',
+      AppleLanguages: ['fr-FR', 'en-US'],
+    },
+  }
+  return RN
+})
+
 describe('ConnectionScreenTests', () => {
   const navigation = jest.fn() as unknown as NativeStackScreenProps<AuthStackParamList, 'Connection'>['navigation']
   const route = jest.fn() as unknown as NativeStackScreenProps<AuthStackParamList, 'Connection'>['route']
@@ -15,6 +27,6 @@ describe('ConnectionScreenTests', () => {
         route={route}
       />
     )
-    expect(screen.getByText('Subscription'))
+    expect(screen.getByText('Connectez-vous pour continuer'))
   })
 })
