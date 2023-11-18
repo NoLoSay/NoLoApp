9bdaf022b392effe9e154c8d7764b2eb1f81e9ce
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-11-18 14:47:41 -0500

    Fix merge conflict

--- src/screens/settings/SettingsScreen.tsx
+++ src/screens/settings/SettingsScreen.tsx
@@ -16,7 +16,6 @@ import { colors } from '@global/colors'
  */
 export default function AddScreen(): React.JSX.Element {
   return (
-<<<<<<< Updated upstream
     <SafeAreaView
       style={{
         backgroundColor: colors.accent,
@@ -28,73 +27,6 @@ export default function AddScreen(): React.JSX.Element {
       <View>
         <Text>Settings</Text>
       </View>
-=======
-    <SafeAreaView style={styles.container}>
-      <TopBar navigation={navigation} />
-      <ScrollView style={styles.scrollview}>
-        <MainInfos
-          accountImage={account.image}
-          firstName={firstName}
-          lastName={lastName}
-          username={username}
-          showModal={showModal}
-        />
-        <View style={{ marginTop: 32, borderRadius: 8, backgroundColor: colors.white }}>
-          <SettingCategoryDisplay
-            title='Mon compte'
-            subtitle='Faire des changements sur mon compte'
-            onPress={() => console.log('account settings')}
-            icon={images.icons.outline.community()}
-          />
-          <SettingCategoryDisplay
-            title='Biométrie'
-            subtitle='Se connecter avec des données biométriques'
-            icon={images.icons.outline.carousel()}
-            childrenIcon={
-              <Switch
-                trackColor={{ false: colors.veryLightGrey, true: colors.accent }}
-                thumbColor={!isBiometryEnabled ? colors.darkGrey : colors.darkGrey}
-                ios_backgroundColor={colors.veryLightGrey}
-                onValueChange={toggleBiometry}
-                value={isBiometryEnabled}
-              />
-            }
-          />
-          <SettingCategoryDisplay
-            title='Se déconnecter'
-            onPress={logoutUser}
-            backIconColor={colors.veryLightGrey}
-            iconColor={colors.darkGrey}
-            icon={images.icons.outline.camera()}
-          />
-        </View>
-        <View style={{ marginTop: 32, borderRadius: 8, backgroundColor: colors.white }}>
-          <SettingCategoryDisplay
-            title='Aide et support'
-            onPress={() => console.log('Help and support')}
-            icon={images.icons.outline.library()}
-          />
-          <SettingCategoryDisplay
-            title="À propos de l'application"
-            onPress={() => console.log('About the app')}
-            icon={images.icons.outline.magnifier()}
-          />
-          <SettingCategoryDisplay
-            title='CGU'
-            onPress={() => console.log('CGU')}
-            icon={images.icons.outline.mapArrow()}
-          />
-        </View>
-      </ScrollView>
-      <ModalView
-        account={account}
-        hideModal={hideModal}
-        setFirstName={setFirstName}
-        setLastName={setLastName}
-        setUsername={setUsername}
-        isVisible={isModalVisible}
-      />
->>>>>>> Stashed changes
     </SafeAreaView>
   )
 }
