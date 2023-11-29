f7b15310cbe6358ee0571088a0a0a9350a5cc8e1
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-11-29 09:44:00 -0500

    Fix tests

--- __tests__/screens/addSection/AddScreen.test.tsx
+++ __tests__/screens/addSection/AddScreen.test.tsx
@@ -2,6 +2,18 @@ import React from 'react'
 import { render } from '@testing-library/react-native'
 import ActionsScreen from '@source/screens/addSection/ActionsScreen/ActionsScreen'
 
+const mockedNavigate = jest.fn()
+
+jest.mock('@react-navigation/native', () => {
+  const actualNav = jest.requireActual('@react-navigation/native')
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      navigate: mockedNavigate,
+    }),
+  }
+})
+
 describe('AddScreenTests', () => {
   it('should render correctly', () => {
     const screen = render(<ActionsScreen />)
