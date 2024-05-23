c626d974203c1dacbc2f82bb9a37be85f506bbc5
Johan Chrillesen <johan.chrillesen@epitech.eu>
2024-05-23 05:21:12 -0400

    Nolo 382 send videos (#56)
    * Add send button
    
    * use RNfetchBlob to send videos
    
    * Fix library display screen
    
    * Prettified Code!
    
    * Update video helper functions and remove unused code
    
    * feat: Add DEV_VIDEO_API_URL to env.d.ts and useActionsScreenController.ts
    
    - Added DEV_VIDEO_API_URL to the declaration module in env.d.ts
    - Updated useActionsScreenController.ts to display an error message and navigate to the SettingsModal when a user with insufficient elevation tries to access the PlacesNeedingTranslation screen
    
    Co-authored-by: JohanCDev <JohanCDev@users.noreply.github.com>
    
    * feat: Update email and password initial values in useConnectionController
    
    - Updated the initial value of the email state variable to 'User' when in development mode
    - Removed unused import statements
    
    Co-authored-by: JohanCDev <JohanCDev@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: JohanCDev <JohanCDev@users.noreply.github.com>

--- src/screens/addSection/PlacesNeedTranslation/usePlacesNeedingTranslationController.ts
+++ src/screens/addSection/PlacesNeedTranslation/usePlacesNeedingTranslationController.ts
@@ -9,7 +9,7 @@ import usePlacesNeedingDescription from '@helpers/httpClient/queries/places/useP
 import { AccountContext } from '@global/contexts/AccountProvider'
 import { launchImageLibrary } from 'react-native-image-picker'
 import { Alert, Linking } from 'react-native'
-import RNFetchBlob from 'rn-fetch-blob'
+import useSendVideo from '@helpers/httpClient/queries/videos/useSendVideo'
 
 /**
  * @typedef {Object} usePlacesNeedingTranslationControllerType
@@ -22,7 +22,7 @@ import RNFetchBlob from 'rn-fetch-blob'
 type usePlacesNeedingTranslationControllerType = {
   onCreatePress: (textToTranslate: string) => void
   onTextPress: (textToTranslate: string, artName: string) => void
-  onSendPress: () => void
+  onSendPress: (id: string) => void
   artPieces: ArtToTranslate[]
   displayError: boolean
   errorText: string
@@ -53,6 +53,10 @@ export default function usePlacesNeedingTranslationController({
     setArtPieces,
     displayErrorModal,
     token: account.accessToken,
+  })
+  const sendVideoMutation = useSendVideo({
+    token: account.accessToken,
+    navigation,
   })
 
   /**
@@ -113,12 +117,10 @@ export default function usePlacesNeedingTranslationController({
           },
         ]
       )
-    } else if (!libraryAlert) {
-      Alert.alert('Erreur', 'Une erreur est survenue lors de la sélection de la vidéo')
     }
   }
 
-  const onSendPress = async () => {
+  const onSendPress = async (id: string) => {
     const res = await launchImageLibrary({
       mediaType: 'video',
     })
@@ -129,38 +131,10 @@ export default function usePlacesNeedingTranslationController({
 
     displayAlert(res.errorCode === 'permission')
 
-    if (res.assets !== undefined && res.assets[0].uri) {
-      const res2 = await RNFetchBlob.fetch(
-        'POST',
-        'http://localhost:3002/upload/1',
-        {
-          'Content-Type': 'multipart/form-data',
-          Authorization: `Bearer ${account.accessToken}`,
-        },
-        [
-          {
-            name: 'file',
-            filename: res.assets[0].fileName,
-            data: JSON.stringify({
-              file: RNFetchBlob.wrap(res.assets[0].uri),
-            }),
-          },
-        ]
-      )
-
-      if (res2.respInfo.status % 100 < 100) {
-        Alert.alert('Succès', 'La vidéo a bien été envoyée', [
-          {
-            text: 'OK',
-            onPress: () => {
-              navigation.goBack()
-              navigation.navigate('Add')
-            },
-          },
-        ])
-      } else {
-        console.error(res2.respInfo.status)
-      }
+    if (res.assets !== undefined && res.assets[0].uri && res.assets[0].fileName) {
+      sendVideoMutation.mutate({
+        variables: { artworkId: id, filename: res.assets[0].fileName, uri: res.assets[0].uri },
+      })
     }
   }
 
