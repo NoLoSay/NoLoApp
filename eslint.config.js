73a2c0114dcf3d85963d5b98ba350d1d2bc7ea2b
Johan Chrillesen <johan.chrillesen@epitech.eu>
2024-06-07 11:37:07 +0200

    chore: Add globals package for eslint configuration

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import globals from "globals";
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        myCustomGlobal: "readonly"
+      }
+    }
+    // ...other config
+  }
+];
